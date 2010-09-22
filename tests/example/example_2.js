1e3bab85e96412d68bfb6a2898ac360f74e8c927
Tobias Stoeckler <mattenmann2003@gmx.de>
2010-09-22 17:32:46 +0000

    #719896 followup: commit files missed in previous commit

---
+++ tests/example/example_2.js
@@ -0,0 +1,19 @@
+// $Id$
+
+/**
+ * @file
+ * Test JavaScript file for Libraries loading.
+ *
+ * Insert a 'libraries-test' div and some text below the page title. See
+ * example_installed.txt for more information.
+ */
+
+(function ($) {
+
+Drupal.behaviors.librariesTest = {
+  attach: function(context, settings) {
+    $('h1#page-title').after('<div id="libraries-test">If this text shows up, the JavaScript file was loaded successfully. If this text is green, the CSS file was loaded successfully.</div>')
+  }
+};
+
+})(jQuery);
---
+++ tests/example/example_3.php
@@ -0,0 +1,13 @@
+<?php
+// $Id$
+
+/**
+ * @file
+ * Test PHP file for Libraries loading.
+ */
+
+/**
+ * Dummy function to see if this file was loaded.
+ */
+function _libraries_test_example_3() {
+}
