e38d203cb547005294868e837456579d03b8ceaf
Tobias Stöckler <tobiasstoeckler@googlemail.com>
2013-11-07 12:27:04 +0100

    Issue #1876124 by tstoeckler: Prepare for adding a test theme.

--- tests/libraries/example/example_3.php
+++ tests/libraries/example/example_3.php
@@ -5,11 +5,8 @@
  * Test PHP file for Libraries loading.
  */
 
-// @see _libraries_require_once()
-$path = 'abc';
-
 /**
  * Dummy function to see if this file was loaded.
  */
-function _libraries_test_example_2() {
+function _libraries_test_module_example_3() {
 }
